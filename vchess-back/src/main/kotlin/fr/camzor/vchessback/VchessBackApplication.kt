705684820ccb58358a37d5aae2930c43f410bfe1
Camzor <camzor@yopmail.com>
2018-12-17 20:56:57 +0100

    Init backend

---
+++ vchess-back/src/main/kotlin/fr/camzor/vchessback/VchessBackApplication.kt
@@ -0,0 +1,12 @@
+package fr.camzor.vchessback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VchessBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<VchessBackApplication>(*args)
+}
+
